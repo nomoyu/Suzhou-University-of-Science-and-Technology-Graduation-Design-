edc63432f4ec625f0c408c45332e6f49b35fa491
Liumy <2900945044@qq.com>
2023-03-12 15:29:18 +0800

    导入功能

--- resource/ruoyi-ui/src/views/business/papers/index.vue
+++ resource/ruoyi-ui/src/views/business/papers/index.vue
@@ -78,6 +78,16 @@
       </el-col>
       <el-col :span="1.5">
         <el-button
+            type="info"
+            plain
+            icon="el-icon-upload"
+            size="mini"
+            @click="handleImport"
+            v-hasPermi="['system:user:import']"
+        >导入</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
           type="warning"
           plain
           icon="el-icon-download"
@@ -180,11 +190,46 @@
         <el-button @click="cancel">取 消</el-button>
       </div>
     </el-dialog>
+
+    <!-- 论文导入对话框 -->
+    <el-dialog :title="upload.title" :visible.sync="upload.open" width="400px" append-to-body>
+      <el-upload
+          ref="uploadRef"
+          :limit="1"
+          accept=".xlsx, .xls"
+          :headers="upload.headers"
+          :action="upload.url + '?updateSupport=' + upload.updateSupport"
+          :disabled="upload.isUploading"
+          :on-progress="handleFileUploadProgress"
+          :on-success="handleFileSuccess"
+          :auto-upload="false"
+          drag
+      >
+        <el-icon class="el-icon--upload"><upload-filled /></el-icon>
+        <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+        <template #tip>
+          <div class="el-upload__tip text-center">
+            <div class="el-upload__tip">
+              <el-checkbox v-model="upload.updateSupport" />是否更新已经存在的用户数据
+            </div>
+            <span>仅允许导入xls、xlsx格式文件。</span>
+            <el-link type="primary" :underline="false" style="font-size:12px;vertical-align: baseline;" @click="importTemplate">下载模板</el-link>
+          </div>
+        </template>
+      </el-upload>
+      <template #footer>
+        <div class="dialog-footer">
+          <el-button type="primary" @click="submitFileForm">确 定</el-button>
+          <el-button @click="upload.open = false">取 消</el-button>
+        </div>
+      </template>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 import { listPapers, getPapers, delPapers, addPapers, updatePapers } from "@/api/business/papers";
+import {getToken} from "@/utils/auth";
 
 export default {
   name: "Papers",
@@ -238,6 +283,20 @@ export default {
         level: [
           { required: true, message: "论文分类不能为空", trigger: "change" }
         ]
+      },
+      upload:{
+        // 是否显示弹出层（用户导入）
+        open: false,
+        // 弹出层标题（用户导入）
+        title: "",
+        // 是否禁用上传
+        isUploading: false,
+        // 是否更新已经存在的用户数据
+        updateSupport: 0,
+        // 设置上传的请求头部
+        headers: { Authorization: "Bearer " + getToken() },
+        // 上传的地址
+        url: process.env.VUE_APP_BASE_API + "/business/papers/importData"
       }
     };
   },
@@ -333,13 +392,40 @@ export default {
         this.$modal.msgSuccess("删除成功");
       }).catch(() => {});
     },
+    /** 导入按钮操作 */
+    handleImport() {
+      this.upload.title = "用户导入";
+      this.upload.open = true;
+    },
     /** 导出按钮操作 */
     handleExport() {
       this.download('business/papers/export', {
         ...this.queryParams
       }, `papers_${new Date().getTime()}.xlsx`)
-    }
-  }
+    },
+    /** 文件上传成功处理 */
+    handleFileSuccess(response, file, fileList){
+      this.upload.open = false;
+      this.upload.isUploading = false;
+      this.$refs["uploadRef"].handleRemove(file);
+      this.$alert("<div style='overflow: auto;overflow-x: hidden;max-height: 70vh;padding: 10px 20px 0;'>" + response.msg + "</div>", "导入结果", { dangerouslyUseHTMLString: true });
+      this.getList();
+    },
+     /**文件上传中处理 */
+    handleFileUploadProgress(event, file, fileList){
+      this.upload.isUploading = true;
+    },
+    /** 提交上传文件 */
+    submitFileForm(){
+      this.$refs["uploadRef"].submit();
+    },
+    /** 下载模板操作 */
+    importTemplate() {
+      this.download("business/papers/importTemplate", {
+      }, `papers_template_${new Date().getTime()}.xlsx`);
+    },
+  },
+
 };
 </script>
 <style>
